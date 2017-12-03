cf60166d8f790713f698b8eedcc085683f219a85
Joe Chang <>
2017-12-03 12:00:45 +0800

    admin read fuction

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -3,5 +3,12 @@ class Admin::RestaurantsController < ApplicationController
 	before_action	:authenticate_admin
 
 	def index
+		@allrestaurants=Restaurant.all
 	end
+
+	def new
+		@newrestaurant=Restaurant.new
+	end
+
+
 end
