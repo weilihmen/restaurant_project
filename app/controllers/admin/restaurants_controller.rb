ae043c30f7118abed6146cff69156b6beeafd9d3
Joe Chang <>
2017-12-03 12:41:27 +0800

    admin new fuction and model validation

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -3,12 +3,28 @@ class Admin::RestaurantsController < ApplicationController
 	before_action	:authenticate_admin
 
 	def index
-		@allrestaurants=Restaurant.all
+		@all_restaurants=Restaurant.all
 	end
 
 	def new
-		@newrestaurant=Restaurant.new
+		@new_restaurant=Restaurant.new
 	end
 
+	def create
+    @new_restaurant = Restaurant.new(restaurant_params) #實例變數必須和new相同，這樣才能回傳error訊息回去render
+    if @new_restaurant.save
+      flash[:notice] = "(notice)儲存成功"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "(alert)儲存失敗"
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+	validates_presence_of(:name,:tel,:address,:opening_hours)
 end
