af668ad8cfc374317bc43b3cbad3cbc3d7c78b6c
Joe Chang <>
2017-12-03 11:47:20 +0800

    authenticate_admin controller and model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_admin
+  	unless current_user.admin?
+  		flash[:alert]="非管理員(Not Allow!)，redirect_to root_path"
+  		redirect_to root_path  		
+  	end
+  end
+
 end
