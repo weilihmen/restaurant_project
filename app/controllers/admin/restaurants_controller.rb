0b25d4b301912a2e7f7729d9edbe31a50588454a
Joe Chang <>
2017-12-03 14:30:13 +0800

    update destroy controller and debug partial form

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -13,17 +13,39 @@ class Admin::RestaurantsController < ApplicationController
 	def create
     @new_restaurant = Restaurant.new(restaurant_params) #實例變數必須和new相同，這樣才能回傳error訊息回去render
     if @new_restaurant.save
-      flash[:notice] = "(notice)儲存成功"
+      flash[:notice] = "(notice)新增成功"
       redirect_to admin_restaurants_path
     else
-      flash.now[:alert] = "(alert)儲存失敗"
+      flash.now[:alert] = "(alert)新增失敗"
       render :new
     end
   end
 
-  def edit
+  def edit #實例變數必須和new相同(使用相同的partial form)
     @new_restaurant=Restaurant.find(params[:id])
   end
+
+  def update #實例變數必須和new相同(使用相同的partial form)
+    @new_restaurant = Restaurant.find(params[:id])
+    if @new_restaurant.update(restaurant_params)
+      flash[:notice] = "(notice)更新成功"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "(alert)更新失敗"
+      render :edit
+    end
+  end
+
+  def destroy
+    @find_restaurant = Restaurant.find(params[:id])
+    if @find_restaurant.destroy
+      flash[:notice] = "(notice)刪除成功"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "(alert)刪除失敗"
+      render :edit
+    end
+end
 
   private
 
