5f99a35fedf9390b312354e4412ad6213070bbdf
Joe Chang <>
2017-12-06 22:10:19 +0800

    photo upload to view show and form and revise controller

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -54,7 +54,7 @@ end
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
   end
 
 end
