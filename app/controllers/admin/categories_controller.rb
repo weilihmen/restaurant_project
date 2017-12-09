f90d4f79b1f2c70c921c0a6ee72a94524cfd2758
Joe Chang <>
2017-12-09 20:50:16 +0800

    category index view have create function

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,5 +4,22 @@ class Admin::CategoriesController < ApplicationController
 
   def index
   	@categories=Category.all
+  	@category=Category.new
   end
+
+  def create
+	  @category = Category.new(category_params)
+	  if @category.save
+	    flash[:notice] = "category was successfully created"
+	    redirect_to admin_categories_path
+	  else
+	    @categories = Category.all #失敗時要把Categories回傳給index
+	    render :index
+	   end
+  end
+
+  private
+  def category_params
+  	params.require(:category).permit(:name)
+	end
 end
