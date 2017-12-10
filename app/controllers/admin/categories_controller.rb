284de8ee11c0b3fcebc0ee32e9f798e1bfb199ab
Joe Chang <>
2017-12-10 14:06:44 +0800

    admin category update controller and index view

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,9 +3,24 @@ class Admin::CategoriesController < ApplicationController
   before_action :authenticate_admin
 
   def index
-  	@categories=Category.all
-  	@category=Category.new
+  	@categories = Category.all
+	  if params[:id] #確認是否有id=?傳入網頁，因為是共用同一個submmit按鈕
+	    @category = Category.find(params[:id])
+	  else
+	    @category = Category.new
+	  end
   end
+
+  def update
+	  @category = Category.find(params[:id])
+	  if @category.update(category_params)
+	    redirect_to admin_categories_path
+	    flash[:notice] = "category was successfully updated"
+	  else
+	    @categories = Category.all
+	    render :index
+	  end
+	end
 
   def create
 	  @category = Category.new(category_params)
@@ -15,7 +30,7 @@ class Admin::CategoriesController < ApplicationController
 	  else
 	    @categories = Category.all #失敗時要把Categories回傳給index
 	    render :index
-	   end
+	  end
   end
 
   private
