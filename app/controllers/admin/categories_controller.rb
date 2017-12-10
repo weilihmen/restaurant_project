186c4316b7c482ebc23049d861f5d30f9019b109
Joe Chang <>
2017-12-10 15:39:53 +0800

    admon category delete controller and index view

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -33,6 +33,14 @@ class Admin::CategoriesController < ApplicationController
 	  end
   end
 
+   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:alert] = "category was successfully deleted"
+    redirect_to admin_categories_path
+  end
+
+
   private
   def category_params
   	params.require(:category).permit(:name)
