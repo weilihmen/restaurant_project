8c58789ca20b221c32590b427c90ce995ef740fc
Joe Chang <>
2017-12-17 15:16:17 +0800

    restaurant show page can submmit comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,10 @@ class CommentsController < ApplicationController
     @comment.restaurant = @restaurant
     @comment.user = current_user
     if @comment.save
-      flash[:notice] = "(notice)新增成功"
+      flash[:notice] = "(notice)Comment新增成功"
       redirect_to restaurant_path(@restaurant)
     else
-      flash.now[:alert] = "(alert)新增失敗"
+      flash.now[:alert] = "(alert)Comment新增失敗"
       render :new
     end
   end
