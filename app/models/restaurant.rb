8d9c0f07cedeb979ae6baea13ddd75a606f29dd4
Joe Chang <>
2017-12-30 21:15:04 +0800

    set like model and association to users and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,11 @@ class Restaurant < ApplicationRecord
 	validates_presence_of(:name,:tel,:address,:opening_hours)
 	mount_uploader :image, PhotoUploader
 	belongs_to :category
-	has_many :favorites
-	has_many :comments
+	#喜歡、收藏餐廳 （刪除餐廳時會刪除喜歡的紀錄）
+	has_many :favorites, dependent: :destroy
+	#留言
+	has_many :comments, dependent: :destroy
+	#喜歡
+	has_many :likes, dependent: :destroy
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :comments
-  has_many :favorites
-  has_many :restaurants, through: :favorites
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
+
+  #留言
+  has_many :comments, dependent: :destroy
+  #最愛、收藏
+  has_many :favorites, dependent: :destroy
+  #透過收藏 取得餐廳 User.restarants
+  has_many :restaurants, through: :favorites
+  #喜歡
+  hes_many :likes, dependent: :destroy
+  has_many :liked_restaurants, through: :likes, source: :restaurant
+  #使用source更名，可以直接讀取 User.liked_restaurants
+
 
   mount_uploader :avatar, AvatarImageUploader
 
-   def admin?
+  def admin?
    	self.role == "admin"
-   end
+  end
 
 end
