4d887135ca799445f94d218a1c564d4d3a26e561
Joe Chang <>
2018-01-06 23:00:08 +0800

    followship model association setting

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,2 +1,9 @@
 class Followship < ApplicationRecord
+	belongs_to :user
+  belongs_to :following, class_name: "User" 
+  #followship裡面有個following id，但沒有following這個model，所以要用class_name把這個id指向USER model
+  validates :following_id, uniqueness: { scope: :user_id }
+  #驗證同樣的following_id 如果有同樣的user_id就不能存入 #validates :name, uniqueness: true
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,14 @@ class User < ApplicationRecord
   has_many :liked_restaurants, through: :likes, source: :restaurant
   #使用source更名，可以直接讀取 User.liked_restaurants
 
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships 
+  #指回following，user.following指這個user正在追蹤的人(following)
+
+  #follower 可以從user取得 追蹤自己的人(follower)
+  has_many :inverse_followships, class_name:"Followship", foreign_key:"following_id"
+  has_many :followers, through: :inverse_followships, source: :user
+  #先自建一個inverse followships，指定followship model的following_id (對USER來說 USER是PK，指去followship的FK是following_id)
 
   mount_uploader :avatar, AvatarImageUploader
 
