a2091c6ff5d87c7a27ec64fa1b69f3191d3b0431
Joe Chang <>
2018-01-07 23:17:30 +0800

    fix feeds page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,13 +6,13 @@ class FavoritesController < ApplicationController
 		@favorite.restaurant_id=@restaurant.id
 		@favorite.save!
 		@restaurant.rank_count
-		redirect_to restaurant_path(@restaurant)
+		redirect_back(fallback_location: root_path)
 	end
 	def destroy
 		@restaurant=Restaurant.find(params[:restaurant_id])
 		@favorite=Favorite.where(:restaurant_id => @restaurant.id, :user_id => current_user.id )
 		@favorite.destroy_all
 		@restaurant.rank_count
-		redirect_to restaurant_path(@restaurant)
+		redirect_back(fallback_location: root_path)
 	end
 end
