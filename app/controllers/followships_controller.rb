93da54729dc2642c36522c7c167b1faf9cc6a761
Joe Chang <>
2018-01-07 21:15:24 +0800

    follow user in profile

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -3,17 +3,14 @@ class FollowshipsController < ApplicationController
     @followship = current_user.followships.new(following_id: params[:following_id])
 
     if @followship.save
-      flash[:notice] = "Successfully followed"
       redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = @followship.errors.full_messages.to_sentence
       redirect_back(fallback_location: root_path)
     end
   end
   def destroy
     @followship = current_user.followships.where(following_id: params[:id]).first
     @followship.destroy
-    flash[:alert] = "Followship destroyed"
     redirect_back(fallback_location: root_path)
   end
 end
