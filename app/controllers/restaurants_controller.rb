9bc64158bf4c2996d94b0011cd4226b6806f4cfa
Joe Chang <>
2018-01-09 22:43:58 +0800

    fix

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,28 +1,28 @@
 class RestaurantsController < ApplicationController
-	
-	def index
-		@restaurants=Restaurant.page(params[:page]).per(9)
-		@categories=Category.all
-	end
-	def show
-		@restaurant = Restaurant.find(params[:id])
-		@favorite=Favorite.where( :restaurant_id => @restaurant, :user_id => current_user )
-		@comment = Comment.new
-		if current_user.restaurants.exists?(:id => params[:id])
-			@favorite_check=:True
-		else
-			@favorite_check=:False
-		end
-	end
-	def feeds
-		@categories=Category.all
+
+  def index
+    @restaurants=Restaurant.page(params[:page]).per(9)
+    @categories=Category.all
+  end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @favorite=Favorite.where( :restaurant_id => @restaurant, :user_id => current_user )
+    @comment = Comment.new
+    if current_user.restaurants.exists?(:id => params[:id])
+      @favorite_check=:True
+    else
+      @favorite_check=:False
+    end
+  end
+  def feeds
+    @categories=Category.all
     @recent_restaurants = Restaurant.order(created_at: :desc).limit(10)
     @recent_comments = Comment.order(created_at: :desc).limit(10)
     @ranks = Restaurant.order(rank: :desc).limit(10)
   end
   def experts
-  	@categories=Category.all
-  	@most_comments=User.joins(:comments).group("users.id").order("count(users.id) DESC").limit(10)
+    @categories=Category.all
+    @most_comments=User.joins(:comments).group("users.id").order("count(users.id) DESC").limit(10)
     @most_followers=User.joins(:followers).group("users.id").order("count(users.id) DESC").limit(10)
   end
 
